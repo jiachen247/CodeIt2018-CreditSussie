b989c4a3331f96f297befdcfc702f3b42c3abae4
gb3h <gabriel.yfy@gmail.com>
2018-09-22 23:57:24 +0800

    'a'

--- codeitsuisse/routes/photogps2.py
+++ codeitsuisse/routes/photogps2.py
@@ -1,9 +1,5 @@
-from GPSPhoto import gpsphoto
-import exifread
-import logging
-
+import exifread as ef
 from flask import request, jsonify;
-
 from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
@@ -11,18 +7,63 @@ logger = logging.getLogger(__name__)
 def evaluate_imagesGPS():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    pathlist = data.get();
+    pathlist = data.get()
+    print('Input:', pathlist)
     # pathlist = [{'path': "https://cis2018-photo-gps.herokuapp.com/images/sample1.jpg"},
     # {'path': "https://cis2018-photo-gps.herokuapp.com/images/sample2.jpg"},
     # {'path': "https://cis2018-photo-gps.herokuapp.com/images/sample3.jpg"},
     # {'path': "https://cis2018-photo-gps.herokuapp.com/images/sample4.jpg"},
-    # {'path': "https://cis2018-photo-gps.herokuapp.com/images/sample5.jpg"}]  
+    # {'path': "https://cis2018-photo-gps.herokuapp.com/images/sample5.jpg"}] 
+    def _convert_to_degress(value):
+        """
+        Helper function to convert the GPS coordinates stored in the EXIF to degress in float format
+        :param value:
+        :type value: exifread.utils.Ratio
+        :rtype: float
+        """
+        d = float(value.values[0].num) / float(value.values[0].den)
+        m = float(value.values[1].num) / float(value.values[1].den)
+        s = float(value.values[2].num) / float(value.values[2].den)
+
+        return d + (m / 60.0) + (s / 3600.0)
+    def getGPS(filepath):
+        '''
+        returns gps data if present other wise returns empty dictionary
+        '''
+        with open(filepath, 'r') as f:
+            tags = ef.process_file(f)
+            latitude = tags.get('GPS GPSLatitude')
+            latitude_ref = tags.get('GPS GPSLatitudeRef')
+            longitude = tags.get('GPS GPSLongitude')
+            longitude_ref = tags.get('GPS GPSLongitudeRef')
+            if latitude:
+                lat_value = _convert_to_degress(latitude)
+                if latitude_ref.values != 'N':
+                    lat_value = -lat_value
+            else:
+                return {}
+            if longitude:
+                lon_value = _convert_to_degress(longitude)
+                if longitude_ref.values != 'E':
+                    lon_value = -lon_value
+            else:
+                return {}
+            return {'latitude': lat_value, 'longitude': lon_value}
+        return {} 
     lst = []
     for x in pathlist:
-        url = x['path']
-        data = gpsphoto.getRawData(url)
-        lst.append(data[tag])
+        filepath = x['path']
+        gps = getGPS(filepath)
+        print(gps)
+        lst.append(gps)
+    print(lst)
     result = lst
-    print(result)
     logging.info("My result :{}".format(result))
-    return jsonify(result);
+    return jsonify(result)
+# evaluate_imagesGPS()
+
+
+
+
+
+
