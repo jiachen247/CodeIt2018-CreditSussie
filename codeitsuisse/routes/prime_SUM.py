426b619e0cf393f2b395d0185b8a592f7302c8b0
gb3h <gabriel.yfy@gmail.com>
2018-09-22 14:41:37 +0800

    FUCKING GO

--- codeitsuisse/routes/prime_SUM.py
+++ codeitsuisse/routes/prime_SUM.py
@@ -7,7 +7,6 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 @app.route('/prime-sum', methods=['POST','GET'])
 def evaluate_prime_sum():
-    print('test')
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input");
@@ -20,35 +19,18 @@ def evaluate_prime_sum():
             return True
         else:
             return False
-    lst = []
-    for x in range(2, inputValue + 1):
-        if is_prime(x):
-            lst.append(x)
-    def helper(n, lst, primes):
-        if n < 0:
-            return None
-        elif n == 0:
-            return primes
-        elif len(lst) == 0:
-            return None
+    pris = []
+    def primer(inp):
+        if is_prime(inp):
+            pris.append(inp)
+            return result
+        elif is_prime(inp - 2):
+            pris.append(inp - 2)
+            pris.append(2)
         else:
-            x = lst[0]
-            primecopy = primes[:]
-            primecopy.append(lst[0])
-            primelst.append(helper(n - x, lst[1:], primecopy))
-            primelst.append(helper(n, lst[1:], primes))     
-    primelst = []
-    helper(inputValue, lst, primelst)
-    def func(lst):
-        z = []
-        for x in lst:
-            if isinstance(x, list):
-                z.append(func(x))
-            elif x != None:
-                z.append(x)
-        return z
-    result = func(primelst)[0]
-    result.reverse()
+            pris.append(3)
+            primer(inp - 3)
+    result = pris
     logging.info("My result :{}".format(result))
     print(result)
     return jsonify(result);
