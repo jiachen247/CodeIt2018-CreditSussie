fa865468840b15c6632585c3155bcd4c9ce1b0d3
gb3h <gabriel.yfy@gmail.com>
2018-09-22 15:13:13 +0800

    asd

---
+++ codeitsuisse/routes/customer.py
@@ -0,0 +1,20 @@
+import logging
+from flask import request, jsonify;
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+@app.route('/customers-and-hotel/minimum-distance', methods=['POST','GET'])
+def evaluate_minimum-distance():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    custs = data.get("input");
+    max_d = 0
+    for x in range(len(custs)):
+        if abs(custs.sorted()[x] - custs.sorted()[x-1]) > max_d:
+            max_d = abs(custs.sorted()[x] - custs.sorted()[x-1])
+        elif abs(custs.sorted()[x] - custs.sorted()[x+1]) > max_d:
+            max_d = abs(custs.sorted()[x] - custs.sorted()[x+1])
+    result = {"answer": max_d}
+    logging.info("My result :{}".format(result))
+    print(result)
+    return jsonify(result);
