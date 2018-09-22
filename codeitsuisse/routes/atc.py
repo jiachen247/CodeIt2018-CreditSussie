aa88f77326b1984bcc353dc9de199800d56e2230
gb3h <gabriel.yfy@gmail.com>
2018-09-23 02:28:16 +0800

    'p'

--- codeitsuisse/routes/atc.py
+++ codeitsuisse/routes/atc.py
@@ -8,27 +8,33 @@ logger = logging.getLogger(__name__)
 @app.route('/airtrafficcontroller', methods=['POST','GET'])
 def evaluate_airtrafficcontroller():
     data = request.get_json()
+    print(data)
     logging.info("data sent for evaluation {}".format(data))
     lof = data.get("Flights")
     secs = data.get("Static")
     seconds = int(secs["ReserveTime"])
-    mins = seconds/60
+    mins = (seconds/60)
     flight_list = []
     for x in lof:
-        time = int(x["Time"])
+        time = x["Time"]
+        minss = int(time[0:2])*60 + int(time[2:4])
         num = x["PlaneId"]
-        flight_list.append([time, num])
+        flight_list.append([minss, num])
     flight_list.sort()
+    print(flight_list)
     for x in range(len(flight_list)-1):
         first_ft = flight_list[x][0]
         second_ft = flight_list[x+1][0]
         if second_ft - first_ft < mins:
-            flight_list[x+1][0] += mins - (second_ft - first_ft)
+            flight_list[x+1][0] += int(mins - (second_ft - first_ft))
     final = []
     for x in flight_list:
         dic = {}
         dic["PlaneId"]=x[1]
-        dic["Time"]=str(int(x[0]))
+        h = str(x[0] // 60).zfill(2)
+        m = str(x[0] % 60).zfill(2)
+        dic["Time"] = h+m
+        print(dic)
         final.append(dic)
     result = {"Flights": final}
     print(result)
