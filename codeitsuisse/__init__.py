f2a4d4c1e54d5769592f3dcb26d3ee2eb0276408
gb3h <gabriel.yfy@gmail.com>
2018-09-22 15:17:25 +0800

    addde cust

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -5,6 +5,7 @@ import codeitsuisse.routes.tallyexpense
 import codeitsuisse.routes.test
 import codeitsuisse.routes.prime_SUM
 import codeitsuisse.routes.photogps
+import codeitsuisse.routes.customer
 
 
 
