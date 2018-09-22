326b6c1fa71b5ede827c09cdfb1245af00517082
jiachen <jiachen247@gmail.com>
2018-09-23 03:02:04 +0800

    djbcjk

--- codeitsuisse/routes/skills.py
+++ codeitsuisse/routes/skills.py
@@ -8,8 +8,8 @@ logger = logging.getLogger(__name__)
 
 @app.route('/skill-tree', methods=['POST','GET'])
 def evaluate_skill():
-    data = request.get_json();
-    print("in : ".format(data))
+    data = request.get_json()
+    print("in : {}".format(data))
     return jsonify({})
 
 
