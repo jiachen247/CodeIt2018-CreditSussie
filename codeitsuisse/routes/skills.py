7645f2bb40c6bb8d8e4ac2390be1147ab13ca64e
jiachen <jiachen247@gmail.com>
2018-09-23 02:57:07 +0800

    1

---
+++ codeitsuisse/routes/skills.py
@@ -0,0 +1,16 @@
+import logging
+
+from flask import request, jsonify;
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/skill-tree', methods=['POST','GET'])
+def evaluate_skill():
+    data = request.get_json();
+    print("in : ".format(data))
+    return jsonify({})
+
+
+
