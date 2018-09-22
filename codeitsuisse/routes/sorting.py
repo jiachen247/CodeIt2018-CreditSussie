ea5595c8e0a3bf10ffa7d27e1e8f6e41a9059d8a
jiachen <jiachen247@gmail.com>
2018-09-22 20:42:40 +0800

    stole code

--- codeitsuisse/routes/sorting.py
+++ codeitsuisse/routes/sorting.py
@@ -3,12 +3,23 @@ import logging
 from flask import request, jsonify
 
 from codeitsuisse import app
-
+from SortingGame import  Board
 logger = logging.getLogger(__name__)
 
 @app.route('/sorting-game', methods=['POST','GET'])
 def evaluate_sorting():
     data = request.get_json()
+
+    input_string =[item for sublist in data for item in sublist]
+    print(input)
+
+
+    b = Board(len(data), input_string)
+    print(b)
+    m = b.get_solution()
+    print(m)
+
+
     print("input: {}".format(data))
     return jsonify({ "result": [6, 8, 5, 6]})
 
