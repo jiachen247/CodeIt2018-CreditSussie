cdbea2c213592a3dcb62792ba663ac6313dbac8d
gb3h <gabriel.yfy@gmail.com>
2018-09-22 16:36:56 +0800

    asd

--- codeitsuisse/routes/customer.py
+++ codeitsuisse/routes/customer.py
@@ -10,37 +10,17 @@ def evaluate_minimum_distance():
     logging.info("data sent for evaluation {}".format(data))
     custs = data;
     custs.sort()
-    print(custs)
-    max_d = 0
     for x in range(len(custs)):
         overflow_val = abs(custs[-1] - custs[0]) - 1
         overflow_index = len(custs)
         y = x + 1
-        if y >= overflow_index:
-            y = y - overflow_index
         if x == 0:
-            if abs(custs[x] - custs[x-1]) - overflow_val <= abs(custs[x] - custs[y]):
-                if abs(custs[x] - custs[x-1]) - overflow_val > max_d:
-                    max_d = abs(custs[x] - custs[x-1]) - overflow_val
-            else:
-                if abs(custs[x] - custs[y]) > max_d:
-                    max_d = abs(custs[x] - custs[y])
-        elif x == overflow_index - 1:
-            if abs(custs[x] - custs[x-1]) <= abs(custs[x] - custs[y]) - overflow_val:
-                if abs(custs[x] - custs[x-1]) > max_d:
-                    max_d = abs(custs[x] - custs[x-1])
-            else:
-                if abs(custs[x] - custs[y]) - overflow_val > max_d:
-                    max_d = abs(custs[x] - custs[y]) - overflow_val
-        else:
-            if abs(custs[x] - custs[x-1]) <= abs(custs[x] - custs[y]):
-                if abs(custs[x] - custs[x-1]) > max_d:
-                    max_d = abs(custs[x] - custs[x-1])
-            else:
-                if abs(custs[x] - custs[y]) > max_d:
-                    max_d = abs(custs[x] - custs[y])
-
-        result = {"answer": max_d}
-    result["answer"] = math.ceil(max_d/2)
+            counter = abs(custs[x] - custs[y])
+        if y >= overflow_index:
+            break
+        if abs(custs[x] - custs[y]) < counter:
+            counter = abs(custs[x] - custs[y])
+    result = {"answer": counter}
+    print(result)
     logging.info("My result :{}".format(result))
     return jsonify(result);
