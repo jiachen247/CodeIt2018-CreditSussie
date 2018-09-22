1f73739b973484d50c4a11ef80586a1570223107
jiachen <jiachen247@gmail.com>
2018-09-23 04:52:57 +0800

    1

--- dev.py
+++ dev.py
@@ -1,17 +1,108 @@
-import pandas as pd
-import numpy as np
-from sklearn import linear_model
+import bisect
 
-sales = [{'x1':1, 'x2': 2, 'x3': 3, 'o': 6},
-         {'x1': 2 ,  'x2': 3, 'x3': 4, 'o': 9},
-         {'x1': 2, 'x2': 1, 'x3': 4, 'o': 7 },
-        {'x1': 5, 'x2': 3, 'x3': 2, 'o': 10 },
-        {'x1': 2, 'x2': 1, 'x3': 2, 'o': 5}
+import array
+
+skills = [
+    {
+      "name": "Grapple Gun",
+      "offense": 5,
+      "points": 1,
+      "require": None
+    },
+    {
+      "name": "Hacking Device",
+      "offense": 6,
+      "points": 2,
+      "require": "Grapple Gun"
+    },
+    {
+      "name": "Remote",
+      "offense": 7,
+      "points": 3,
+      "require": "Hacking Device"
+    },
+    {
+      "name": "Bomb",
+      "offense": 20,
+      "points": 5,
+      "require": "Remote"
+    },
+    {
+      "name": "Inverted takedown",
+      "offense": 5,
+      "points": 1,
+      "require": None
+    },
+    {
+      "name": "Shockwave attack",
+      "offense": 8,
+      "points": 3,
+      "require": "Inverted takedown"
+    }
 ]
 
-df = pd.DataFrame(sales)
+target = 11
 
-reg = linear_model.LinearRegression()
-reg.fit(df[['x1','x2','x3']],df.o)
-prediction = reg.predict([[3, 4, 5]])[0]
+
+
+
+
+
+for skill in skills:
+    name = skill.get("name")
+    require = skill.get("require")
+
+    skillz[name] = skill
+    if require in indexes:
+        indexes[require].append(name)
+    else:
+        indexes[require] = [name]
+
+print(indexes)
+print(skillz)
+# start with the first none
+
+frontier = [Node(skillz.get(index), None , 0, 0) for index in indexes[None]]
+explored = []
+visited = 0
+
+while True:
+
+    if len(frontier) == 0 :
+        break
+
+    node = frontier.pop(0)
+    visited += 1
+    print("visiting : {}".format(node.skill_name))
+
+    if node.total_offense >= target:
+        print(node.ratio)
+        if node.ratio > best_ratio:
+            best_ratio = node.ratio
+            best_skill = node
+
+
+
+    for sk in node.get_next():
+        new_node = Node(skillz.get(sk),node, node.offence, node.points )
+        if new_node not in frontier and new_node not in explored:
+            # use bisect to insert the node at the proper place in the frontier
+            #bisect.insort(frontier, new_node)
+            frontier.insert(0, new_node)
+
+    explored.append(node)
+
+
+
+moves = []
+while True:
+    moves.append(best_skill.skill_name)
+    if best_skill.parent != None:
+        best_skill = best_skill.parent
+    else:
+        break
+moves.reverse()
+print(moves)
+
+
 
